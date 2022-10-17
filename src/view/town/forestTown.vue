45abcf768bff8bfbf215306e9d4c9f25bb8e6b92
July <1114174347@qq.com>
2022-10-17 23:14:42 +0800

    重心应该放在3D模型上，而不是在这里做平面

--- src/view/town/forestTown.vue
+++ src/view/town/forestTown.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { World, Model, ThirdPersonCamera, useKeyboard, useLoop, Skybox } from "lingo3d-vue"
+import { World, Model, ThirdPersonCamera, useKeyboard, useLoop, Skybox,Dummy } from "lingo3d-vue"
 import { ref } from "vue"
 import Music from "../../components/common/Music.vue";
 
@@ -8,34 +8,40 @@ const key = useKeyboard()
 const running = ref(false)
 
 useLoop(() => {
-  if (key.value === "w") {
+  if (RegExp(/w/).test(key.value)) {
     running.value = true
     characterRef.value.moveForward(-6)
-  } else if(key.value === "s"){
+    
+  } else if(RegExp(/s/).test(key.value)){
     running.value = true
     characterRef.value.moveForward(6)
-  } else if (key.value === "a") {
-    running.value = true
-    characterRef.value.moveRight(6)
-  } else if(key.value === "d"){
-    running.value = true
-    characterRef.value.moveRight(-6)
   } else {
     running.value = false
   }
+  
+  if (RegExp(/a/).test(key.value)) {
+    running.value = true
+    characterRef.value.moveRight(6)
+  } else if(RegExp(/d/).test(key.value)){
+    running.value = true
+    characterRef.value.moveRight(-6)
+  }
+  
 })
 </script> 
 
 <template>
   <Music musicSrc='/src/assets/bgm/town/forest_town.ogg' />
   <World>
+    <Model src="bbb.fbx" :scale="1" physics="map" :y="-50" :x='-1700'/>
     <Model src="fairy.glb" :scale="20" physics="map" />
-    <ThirdPersonCamera active mouseControl>
-      <Model
+    <ThirdPersonCamera active mouseControl >
+      <Dummy
         ref="characterRef"
         src="person.glb"
         :y="200"
-        :z="-150"
+        :z="-300"
+        strideMode="free"
         :animations="{ idleAnimation: 'idle.fbx', runningAnimation: 'running.fbx' }"
         physics="character"
         :animation="running ? 'runningAnimation' : 'idleAnimation'"
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import vue from '@vitejs/plugin-vue'
 import {resolve} from 'path'
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: '0.0.0.0'
+  },
   resolve: {
     extensions: ['.vue', '.png','.mp3','.jpg','.ogg','.js'],
     alias: {
